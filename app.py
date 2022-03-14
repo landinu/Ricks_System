838fe741437f8fff2ebe28e113be49bb64d7b398
Uri Raquel Landín Martínez <urilm@piedrita.lan>
2022-03-14 14:39:58 -0600

    Main code modified for local test.

--- app.py
+++ app.py
@@ -19,11 +19,13 @@ from flask_jwt import JWT
 
 from security import authenticate, identity
 from resources.user import User, UserRegister, UserList 
-from resources.rol import Rol, RolRegister, RolList
-from resources.location import Location, LocationRegister, LocationList
-from resources.character import Character, CharacterRegister, CharacterList
-#from resources.object import Object, ObjectRegister,ObjectList
-from resources.multiverse import Multiverse, MultiverseRegister, MultiverseList
+from resources.role import Role, RoleList
+from resources.location import Location, LocationList
+from resources.character import Character, CharacterList
+#from resources.object import Object, ObjectList
+from resources.multiverse import Multiverse, MultiverseList
+from models.role import RoleModel
+from models.user import UserModel
 
 # DESARROLLO APLICACIÓN
 
@@ -43,21 +45,38 @@ jwt = JWT(app,authenticate,identity) # Crea endpoint /auth para la autenticació
 api.add_resource(User,'/user/<string:name>')
 api.add_resource(UserRegister,'/register')
 api.add_resource(UserList,'/users')
-api.add_resource(Rol,'/role/<string:name>')
-api.add_resource(RolRegister,'/new-role')
-api.add_resource(RolList,'/roles')
+api.add_resource(Role,'/role/<string:name>')
+api.add_resource(RoleList,'/roles')
 api.add_resource(Location,'/location/<string:name>')
-api.add_resource(LocationRegister,'/new-location')
 api.add_resource(LocationList,'/locations')
 api.add_resource(Character,'/character/<string:name>')
-api.add_resource(CharacterRegister,'/new-character')
 api.add_resource(CharacterList,'/characters')
 #api.add_resource(Object,'/object/<string:name>')
-#api.add_resource(ObjectRegister,'/new-object')
 #api.add_resource(ObjectList,'/objects')
 api.add_resource(Multiverse,'/multiverse/<string:name>')
-api.add_resource(MultiverseRegister,'/new-multiverse')
 api.add_resource(MultiverseList,'/multiverses')
+
+from db import db
+
+db.init_app(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all() # Unicamente crea las tablas que ve (los archivos que se importan)
+
+    role = RoleModel("admin")
+    role.save_to_db()
+    role = RoleModel("member")
+    role.save_to_db()
+    role = RoleModel("reader")
+    role.save_to_db()
+
+    user = UserModel("Ryck","Ryck123",1)
+    user.save_to_db()
+    user = UserModel("Morty","Morty123",2)
+    user.save_to_db()
+    user = UserModel("Summer","Summer123",3)
+    user.save_to_db()
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True) # debug = True -> Arroja mensajes de error útiles para identificar el problema
